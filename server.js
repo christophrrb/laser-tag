b3b1b68ee8ef6d84178e10cf6a0f16b0468490bb
Denis Veller <denis@Deniss-MacBook-Pro.local>
2017-11-18 18:26:13 -0500

    go back! I SAID GO BACK

--- server.js
+++ server.js
@@ -4,6 +4,3 @@ const serverStatic = require('serve-static');
 const port = process.env.PORT || process.env.port || 8080;
 
 connect().use(serverStatic(__dirname)).listen(port);
-var geomagnetism = require('geomagnetism');
-var info = geomagnetism.model().point([Denis.lat,Denis.long]);
-console.log('declination:', info.decl);
