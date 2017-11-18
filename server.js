331f5ec785892dc0e80627c2e3f1875fedfd367f
Denis Veller <denis@Deniss-MacBook-Pro.local>
2017-11-18 10:07:08 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,6 @@
+const connect = require('connect');
+const serverStatic = require('serve-static');
+
+const port = process.env.PORT || process.env.port || 8080;
+
+connect().use(serverStatic(__dirname)).listen(port);
