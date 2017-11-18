0ed0b0dec6dbd8258b47f715896867c58900dcb7
Denis Veller <denis@Deniss-MacBook-Pro.local>
2017-11-18 18:24:16 -0500

    i have no clue what ive done to server.js

--- server.js
+++ server.js
@@ -5,3 +5,5 @@ const port = process.env.PORT || process.env.port || 8080;
 
 connect().use(serverStatic(__dirname)).listen(port);
 var geomagnetism = require('geomagnetism');
+var info = geomagnetism.model().point([Denis.lat,Denis.long]);
+console.log('declination:', info.decl);
